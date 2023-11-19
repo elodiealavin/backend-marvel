2ef088b3afaf49b4d158532bcd826b550c66bb07
Elodie <elodie@MacBook-Air-de-Elodie.local>
2023-11-19 20:20:50 +0100

    update url route characterId

--- routes/characterId.js
+++ routes/characterId.js
@@ -4,8 +4,9 @@ const axios = require("axios");
 
 router.get("/character/:characterId", async (req, res) => {
   try {
+    const characterId = req.params.characterId;
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/character/${req.params.characterId}?apiKey=${process.env.API_KEY}`
+      `https://lereacteur-marvel-api.herokuapp.com/character/${characterId}?apiKey=${process.env.API_KEY}`
     );
     console.log(response.data);
     res.json(response.data);
