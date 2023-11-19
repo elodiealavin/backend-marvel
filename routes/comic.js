480f42e3c82dd048bcd9c5c3c1efab4e4acfc5df
Elodie <elodie@MacBook-Air-de-Elodie.local>
2023-11-19 20:42:10 +0100

    update variable route comic

--- routes/comic.js
+++ routes/comic.js
@@ -4,9 +4,9 @@ const axios = require("axios");
 
 router.get("/comic/:comicId", async (req, res) => {
   try {
-    // const { comicId } = req.params;
+    const comicId = req.params.comicId;
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/comic/${req.params.comicId}?apiKey=${process.env.API_KEY}`
+      `https://lereacteur-marvel-api.herokuapp.com/comic/${comicId}?apiKey=${process.env.API_KEY}`
     );
     console.log(response.data);
     res.json(response.data);
